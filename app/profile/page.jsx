de81aa7ad730884038c7355d1f575957369666a3
TrongNhan30520 <trongnhan30520@gmail.com>
2023-05-23 12:12:00 +0700

    fix profile page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -5,7 +5,6 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 import Profile from "@components/Profile";
-
 const MyProfile = () => {
   const router = useRouter();
   const { data: session } = useSession();
